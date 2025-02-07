30f835fb0d142095baef299d08bdcb3c3ff8314c
Audio-sohn <info@audiosohn.com>
2025-02-07 10:32:52 +0100

    bubble sort implemented

--- bubblesort/bubbleup.go
+++ bubblesort/bubbleup.go
@@ -6,6 +6,22 @@ package bubblesort
 // Die Funktion gibt true zurück, wenn mindestens ein Tausch durchgeführt wurde.
 func BubbleUp(list []int) bool {
 	swapped := false
-	// TODO
+
+	for i, speci := range list {
+
+		// is specimen bigger than next entry?
+		if i+1 < len(list) && speci > list[i+1] {
+
+			// if so, swap with next entry
+			temp := list[i+1]
+			list[i+1] = speci
+			list[i] = temp
+
+			// set swapped to true
+			swapped = true
+		}
+
+	}
+
 	return swapped
 }
