02b326e3f557b87754df08acb440fa160b176211
Audio-sohn <info@audiosohn.com>
2025-02-14 09:47:20 +0100

    moveleft updated

--- insertionsort/moveleft.go
+++ insertionsort/moveleft.go
@@ -1,7 +1,31 @@
 package insertionsort
 
 // MoveLeft bewegt das Element an der gegebenen Position nach links,
-// bis es an der richtigen Position ist.
+// bis es rechts von der nächstkleineren zahl ist.
 func MoveLeft(list []int, pos int) {
-	// TODO
+
+	// get specimen
+	package insertionsort
+
+import "sortieren/testhelpers"
+
+	// MoveLeft bewegt das Element an der gegebenen Position nach links,
+	// bis es rechts von der nächstkleineren Zahl ist.
+	func MoveLeft(list []int, pos int) {
+
+		swapped := false
+
+		//Überprüfen ob Element an pos kleiner als Element an pos-1 ist
+		// Argument pos > 0 MUSS an erster Stelle stehen, damit zuerst darauf überprüft wird (ansonsten OUT OF RANGE)
+		if pos > 0 && list[pos] < list[pos-1] {
+
+			//list[pos] wird eins nach links verschoben
+			testhelpers.SwapElements(list, pos, pos-1)
+			swapped = true
+		}
+
+		if swapped {
+			MoveLeft(list, pos-1)
+		}
+	}
 }
