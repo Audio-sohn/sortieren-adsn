4c700788a39907a1872157cf21fb1dbcfa70a3e5
Audio-sohn <info@audiosohn.com>
2025-03-02 15:07:54 +0100

    mergesort under construction

--- mergesort/merge.go
+++ mergesort/merge.go
@@ -3,9 +3,18 @@ package mergesort
 // Merge erwartet zwei sortierte Listen und gibt eine sortierte Liste zurück,
 // die alle Elemente der beiden Eingabelisten enthält.
 func Merge(list1, list2 []int) []int {
-	result := []int{}
 
-	// TODO
+	if len(list1) == 0 {
 
-	return result
+		return list2
+
+	} else if len(list2) == 0 {
+
+		return list1
+
+	}
+
+	return nil
+
+	//return append(Merge()
 }
